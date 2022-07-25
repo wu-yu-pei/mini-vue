2655dbd99662fdd2af30eaf5358bd3c4282e9450
Wuyupei <495174699@qq.com>
2022-07-25 16:49:46 +0800

    reactive:isReactive isReadonly

--- src/reactive/reactive.ts
+++ src/reactive/reactive.ts
@@ -1,11 +1,24 @@
 import { mutableHandlers, readonlyHandlers } from './basehandler';
 
+export const enum ReactiveFlag {
+  IS_REACTIVE = '__v-isReactive',
+  IS_READONLY = '__v-isReadonly',
+}
+
 export function reactive(obj) {
   return createActiveObject(obj, mutableHandlers);
 }
 
 export function readonly(obj) {
   return createActiveObject(obj, readonlyHandlers);
+}
+
+export function isReactive(obj) {
+  return !!obj[ReactiveFlag.IS_REACTIVE];
+}
+
+export function isReadonly(obj) {
+  return !!obj[ReactiveFlag.IS_READONLY];
 }
 
 function createActiveObject(obj, bashHandle) {
--- src/reactive/test/reactive.spec.ts
+++ src/reactive/test/reactive.spec.ts
@@ -1,4 +1,4 @@
-import { reactive } from '../reactive';
+import { reactive, isReactive } from '../reactive';
 
 describe('reactive', () => {
   it('happy path', () => {
@@ -6,5 +6,7 @@ describe('reactive', () => {
     const now = reactive(origin);
     expect(now).not.toBe(origin);
     expect(now.foo).toBe(1);
+    expect(isReactive(origin)).toBe(false);
+    expect(isReactive(now)).toBe(true);
   });
 });
