8e84c767ccb6bbbb5b0856549745420eec1504bd
Wuyupei <495174699@qq.com>
2022-07-25 10:56:35 +0800

    effect:scheduler stop

--- src/reactive/effect.ts
+++ src/reactive/effect.ts
@@ -1,16 +1,26 @@
 class ReactiveEffect {
   private _fn: any;
-  constructor(fn) {
+  public scheduler: any;
+  public deps = [];
+  constructor(fn, scheduler?) {
     this._fn = fn;
+    this.scheduler = scheduler;
   }
 
   run() {
     acctiveEffect = this;
     return this._fn();
   }
+
+  stop() {
+    this.deps.forEach((dep: any) => {
+      dep.delete(this);
+    });
+  }
 }
 
 const targetMap = new Map();
+// 依赖收集
 export function track(target, key) {
   let deps = targetMap.get(target);
   if (!deps) {
@@ -24,22 +34,38 @@ export function track(target, key) {
     deps.set(key, dep);
   }
   dep.add(acctiveEffect);
+  // todo 好像有一个bug
+  acctiveEffect && acctiveEffect.deps.push(dep);
 }
 
+// 依赖触发
 export function trigger(target, key) {
   const deps = targetMap.get(target);
   const dep = deps.get(key);
+
   for (const fn of dep) {
-    fn.run();
+    if (fn.scheduler) {
+      fn.scheduler();
+    } else {
+      fn.run();
+    }
   }
 }
 let acctiveEffect;
 
-export function effect(fn) {
+export function stop(runner) {
+  runner.effect.stop();
+}
+// effect函数
+export function effect(fn, options: any = {}) {
   // 调用fn
-  const _effect = new ReactiveEffect(fn);
+  const _effect = new ReactiveEffect(fn, options.scheduler);
 
   _effect.run();
 
-  return _effect.run.bind(_effect);
+  const runner: any = _effect.run.bind(_effect);
+
+  runner.effect = _effect;
+
+  return runner;
 }
--- src/reactive/test/effect.spec.ts
+++ src/reactive/test/effect.spec.ts
@@ -1,5 +1,5 @@
 import { reactive } from '../reactive';
-import { effect } from '../effect';
+import { effect, stop } from '../effect';
 
 describe('effect', () => {
   it('happy path', () => {
@@ -30,5 +30,47 @@ describe('effect', () => {
     const r = runner();
     expect(foo).toBe(3);
     expect(r).toBe('foo');
+  });
+
+  it('effect scheduler', () => {
+    // 1.effect的第二个参数给定的一个scheduler的数据
+    // 2.当effect第一次执行的时候，还会执行发你
+    // 3.当响应式对象发生set时， 不会执行fn 而是去执行 scheduler
+    // 如果当执行runner的时候， 才会去执行fn
+    let dumy;
+    let run: any;
+    const scheduler = jest.fn(() => {
+      run = runner;
+    });
+    const obj = reactive({ foo: 1 });
+    const runner = effect(
+      () => {
+        dumy = obj.foo;
+      },
+      { scheduler }
+    );
+
+    expect(scheduler).not.toHaveBeenCalled();
+    expect(dumy).toBe(1);
+    obj.foo++;
+    expect(dumy).toBe(1);
+    run();
+    expect(dumy).toBe(2);
+  });
+
+  it('stop', () => {
+    let dumy;
+    const obj = reactive({ prop: 1 });
+    const runner = effect(() => {
+      dumy = obj.prop;
+    });
+    obj.prop = 2;
+    expect(dumy).toBe(2);
+    stop(runner);
+    obj.prop = 3;
+    expect(dumy).toBe(2);
+
+    runner();
+    expect(dumy).toBe(3);
   });
 });
